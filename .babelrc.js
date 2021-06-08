3e2d6b9b00860812d34ebda8d1be9cd238972698
TheComputerM <mudit.harshit99@outlook.com>
2021-06-08 12:33:56 +0530

    add ESM support

--- .babelrc.js
+++ .babelrc.js
@@ -1,3 +1,15 @@
 module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-typescript'],
+  env: {
+    esm: {
+      presets: [
+        [
+          '@babel/env',
+          {
+            modules: false,
+          },
+        ],
+      ],
+    },
+  },
 };
