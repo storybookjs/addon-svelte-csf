7e60bb480409bb1db3e34cb39a1c6765bca35b9f
TheComputerM <mudit.harshit99@outlook.com>
2021-06-09 09:27:33 +0530

    fix loading of preset

--- preset.js
+++ preset.js
@@ -1,12 +1,1 @@
-function config(entry = []) {
-  return [...entry, require.resolve("./dist/esm/preset/preview")];
-}
-
-function managerEntries(entry = []) {
-  return [...entry, require.resolve("./dist/esm/preset/manager")];
-}
-
-module.exports = {
-  managerEntries,
-  config,
-};
+module.exports = require('./dist/cjs/preset');
