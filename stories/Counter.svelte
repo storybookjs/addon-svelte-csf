6bb61febecae694e14a4c489d8d47966e9ad515b
j3rem1e <jeremie.brebec@gmail.com>
2022-04-03 20:35:40 +0200

    Upgrade dependencies

---
+++ stories/Counter.svelte
@@ -0,0 +1,10 @@
+<script>
+  let count = 0;
+</script>
+
+<div>
+  <h1>Counter</h1>
+  <h2 data-testid="count">You clicked {count} times</h2>
+  <button type="button" on:click={() => count--}>Decrement</button>
+  <button type="button" on:click={() => count++}>Increment</button>
+</div>
