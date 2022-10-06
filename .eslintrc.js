5d94f755d4c84edb2fadd7e9a9b8b72953ce7a8d
Ian VanSchooten <ian.vanschooten@gmail.com>
2022-10-05 21:18:57 -0400

    Fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,11 @@
 module.exports = {
   root: true,
   extends: ['@storybook/eslint-config-storybook'],
+  rules: {
+    '@typescript-eslint/dot-notation': 'off',
+    '@typescript-eslint/no-implied-eval': 'off',
+    '@typescript-eslint/no-throw-literal': 'off',
+    '@typescript-eslint/return-await': 'off',
+  },
   overrides: [],
 };
