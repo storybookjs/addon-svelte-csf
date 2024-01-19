cfc1e3af32eb7c6819cbb725b328ea057e1da71f
paoloricciuti <ricciutipaolo@gmail.com>
2024-01-19 18:47:19 +0100

    chore: add test for capturing scope in interactions

--- stories/interaction.stories.svelte
+++ stories/interaction.stories.svelte
@@ -4,6 +4,7 @@
   import { userEvent, within } from '@storybook/testing-library';
 
   import Counter from './Counter.svelte';
+  import { tick } from 'svelte';
 
   async function play({ canvasElement }) {
     const canvas = within(canvasElement);
@@ -12,10 +13,26 @@
     const count = await canvas.findByTestId('count');
     expect(count.textContent).toEqual('You clicked 1 times');
   }
+
+  let i = 0;
 </script>
 
 <Meta title="Interactions" component={Counter} />
 
 <Story name="Play" {play}>
   <Counter />
+</Story>
+
+<Story
+  name="Play (capturing scope)"
+  play={async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const p = canvas.getByTestId('count');
+    expect(p.textContent).toEqual('0');
+    i++;
+    await tick();
+    expect(p.textContent).toEqual('1');
+  }}
+>
+  <p data-testid="count">{i}</p>
 </Story>
