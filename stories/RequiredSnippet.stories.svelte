3dd8d622ff254b546dced1ed4a76d75c63fd673b
xeho91 <xeho91@pm.me>
2024-05-25 12:22:12 +0800

    Add example for failing TypeScript case with required snippets

---
+++ stories/RequiredSnippet.stories.svelte
@@ -0,0 +1,29 @@
+<script context="module" lang="ts">
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+
+  import RequiredSnippet from './RequiredSnippet.svelte';
+
+  const { Story } = defineMeta({
+    component: RequiredSnippet,
+    tags: ['autodocs'],
+  });
+</script>
+
+
+<!-- Not working, as expected -->
+<!-- <Story name="Case 1" /> -->
+
+{#snippet children()}
+<p>This works</p>
+{/snippet}
+
+<!-- Works, as expected -->
+<Story name="Case 2" args={{ children }} />
+
+<!-- Works, as expected, but TypeScript is not happy -->
+<Story name="Case 3">
+<!-- Current workaround: add `args: { children: "" }` to `defineMeta`, but this is invalid, Svelte snippets cannot be literal, they're functions --> 
+  {#snippet children()}
+    <p>This works</p>
+  {/snippet}
+</Story>
---
+++ stories/RequiredSnippet.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+
+interface Props {
+  children: Snippet;
+}
+
+  let { children }: Props = $props();
+</script>
+
+<div>
+  {@render children()}
+</div>
