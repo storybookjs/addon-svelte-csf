a242187f5243fec13f2eb32e53e5bac6d6bb89c4
xeho91 <xeho91@pm.me>
2024-05-28 23:12:57 +0800

    Update test config, ignore old

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import inspect from 'vite-plugin-inspect';
 
 export default defineConfig({
+  define: {
+    'import.meta.vitest': 'undefined',
+  },
   plugins: [
     svelte(),
     inspect({
@@ -10,4 +15,10 @@ export default defineConfig({
       build: true,
     }),
   ],
+  test: {
+    dir: './src/',
+    environment: 'jsdom',
+    globals: true,
+    includeSource: ['src/**/*.ts'],
+  },
 });
