040a7c56748d157b6092b197eb2b581eaee115c4
Jeppe Reinhold <jeppe@reinhold.is>
2024-05-29 22:13:44 +0200

    extract identifier tests to separate file

--- src/utils/identifiers.ts
+++ src/utils/identifiers.ts
@@ -26,31 +26,3 @@ export const storyNameToId = (name: string) =>
  * @example storyNameToExportName('Some Long Story Name!') => 'SomeLongStoryName'
  */
 export const storyNameToExportName = (name: string) => storyIdToExportName(storyNameToId(name));
-
-if (import.meta.vitest) {
-  const { it, expect } = import.meta.vitest;
-  it('storyIdToExportName', () => {
-    expect(storyIdToExportName('single')).toBe('Single');
-    expect(storyIdToExportName('multiple-parts')).toBe('MultipleParts');
-  });
-  it('exportNameToStoryId', () => {
-    expect(exportNameToStoryId('Single')).toBe('single');
-    expect(exportNameToStoryId('MultipleParts')).toBe('multiple-parts');
-  });
-  it('storyNameToId', () => {
-    expect(storyNameToId('simple')).toBe('simple');
-    expect(storyNameToId('PascalCase')).toBe('pascal-case');
-    expect(storyNameToId('Start Case')).toBe('start-case');
-    expect(storyNameToId('With 2 illegal !! characters, a PascalCase and an ?')).toBe(
-      'with-2-illegal-characters-a-pascal-case-and-an'
-    );
-  });
-  it('storyNameToExportName', () => {
-    expect(storyNameToExportName('simple')).toBe('Simple');
-    expect(storyNameToExportName('PascalCase')).toBe('PascalCase');
-    expect(storyNameToExportName('Start Case')).toBe('StartCase');
-    expect(storyNameToExportName('With 2 illegal !! characters, a PascalCase and an ?')).toBe(
-      'With2IllegalCharactersAPascalCaseAndAn'
-    );
-  });
-}
