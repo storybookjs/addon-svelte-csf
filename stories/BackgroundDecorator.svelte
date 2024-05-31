98835ab62a2ac351e14d0307ee4b32049ad2c6bf
xeho91 <xeho91@pm.me>
2024-05-31 12:39:54 +0800

    Add example & visual test with decorators

---
+++ stories/BackgroundDecorator.svelte
@@ -0,0 +1,7 @@
+<script>
+let {children,color} = $props();
+</script>
+
+<div style:background-color={color}>
+  {@render children()}
+</div>
---
+++ stories/Decorators.stories.svelte
@@ -0,0 +1,21 @@
+<script context="module">
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+
+  import Text from './Text.svelte';
+  import BackgroundDecorator from './BackgroundDecorator.svelte';
+
+  /** Demonstration on how to use decorators & visual test */
+  const { Story } = defineMeta({
+    component: Text,
+    tags: ['autodocs'],
+    decorators: [() => ({ Component: BackgroundDecorator, props: { color: 'pink' } })],
+  });
+</script>
+
+<Story name="Meta decorator" args={{ text: 'From defineMeta decorators entry' }} />
+
+<Story
+  name="Story decorator"
+  args={{ text: 'from Story decorators argument' }}
+  decorators={[() => ({ Component: BackgroundDecorator, props: { color: 'lightblue' } })]}
+/>
