3f9f461c041b0ef0bf223da5b02ce2ed67402d13
xeho91 <xeho91@pm.me>
2024-06-08 20:53:16 +0800

    fix: imports not working for `*.svelte` files

--- src/runtime/create-runtime-stories.ts
+++ src/runtime/create-runtime-stories.ts
@@ -3,9 +3,9 @@ import { logger } from '@storybook/client-logger';
 import type { Meta, StoryObj } from '@storybook/svelte';
 import { mount, unmount, type ComponentType } from 'svelte';
 
-import StoriesExtractor from '#runtime/StoriesExtractor.svelte';
-import StoryRenderer from '#runtime/StoryRenderer.svelte';
-import type { StoriesRepository } from '#runtime/contexts/extractor.svelte';
+import StoriesExtractor from './StoriesExtractor.svelte';
+import StoryRenderer from './StoryRenderer.svelte';
+import type { StoriesRepository } from './contexts/extractor.svelte';
 
 const createFragment = document.createDocumentFragment
   ? () => document.createDocumentFragment()
