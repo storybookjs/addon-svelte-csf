d0d95f401f51c2e19122a677b54c9f9fa8ca36a1
Jeppe Reinhold <jeppe@reinhold.is>
2024-06-20 22:39:46 +0200

    add "Examples Only" version of Storybook and Chromatic

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,18 +1,20 @@
 import type { StorybookConfig } from '@storybook/svelte-vite';
 
+const examplesOnly = process.env.EXAMPLES_ONLY === 'true';
+
 const config: StorybookConfig = {
   stories: [
     {
       directory: '../examples',
       files: '**/*.stories.@(ts|svelte)',
-      titlePrefix: 'Examples',
+      titlePrefix: examplesOnly ? undefined : 'Examples',
     },
-    {
+    !examplesOnly && {
       directory: '../tests/stories',
       files: '**/*.stories.@(ts|svelte)',
       titlePrefix: 'Tests',
     },
-  ],
+  ].filter(Boolean) as StorybookConfig['stories'],
   framework: '@storybook/svelte-vite',
   addons: ['../dist/preset.js', '@storybook/addon-essentials', '@storybook/addon-interactions'],
 };
