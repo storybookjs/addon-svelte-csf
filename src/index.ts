d98a81e420540b81ea2dc3398e8f277331142548
Jeppe Reinhold <jeppe@reinhold.is>
2024-06-26 00:44:18 +0200

    export unsupoorted components

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,23 @@ import type { Args as BaseArgs } from '@storybook/types';
 import type { EmptyObject } from 'type-fest';
 
 import type {
-  Meta,
+  Meta as MetaType,
   StoryCmp,
   StoryContext as BaseStoryContext,
   StoryAnnotations,
   Cmp,
 } from '#types';
 
-import Story from './runtime/Story.svelte';
+import StoryComponent from './runtime/Story.svelte';
 
 export { setTemplate } from './runtime/contexts/template.svelte';
 
 export function defineMeta<
   const TOverrideArgs extends BaseArgs = EmptyObject,
   const TCmp extends Cmp = Cmp,
->(meta: Meta<TCmp>) {
+>(meta: MetaType<TCmp>) {
   return {
-    Story: Story as StoryCmp<EmptyObject, TCmp, typeof meta>,
+    Story: StoryComponent as StoryCmp<EmptyObject, TCmp, typeof meta>,
     meta,
   };
 }
@@ -32,3 +32,30 @@ export type StoryContext<TStoryCmp> =
   TStoryCmp extends StoryCmp<infer _TOverrideArgs, infer TCmpOrArgs, infer TMeta>
     ? BaseStoryContext<TCmpOrArgs, TMeta>
     : never;
+
+/**
+ * @deprecated Use `defineMeta` instead
+ * @see TODO link to MIGRATION.md
+ */
+export const Meta = () => {
+  throw new Error(`The Meta component has been removed in favor of the defineMeta function.
+    See TODO link to MIGRATION.md for more details.`);
+};
+
+/**
+ * @deprecated Use `Story` component returned from `defineMeta` instead
+ * @see TODO link to MIGRATION.md
+ */
+export const Story = () => {
+  throw new Error(`The Story component can not be imported anymore, but must be desctructured from the defineMeta() call.
+    See TODO link to MIGRATION.md for more details.`);
+};
+
+/**
+ * @deprecated Use snippets instead
+ * @see TODO link to MIGRATION.md
+ */
+export const Template = () => {
+  throw new Error(`The Template component has been removed in favor of the snippets syntax.
+    See TODO link to MIGRATION.md for more details.`);
+};
