fdebf5ca6486d5e08faef98fede1176156a1be2e
Mateusz Kadlubowski <xeho91@pm.me>
2024-07-21 13:13:04 +0800

    update tests

--- src/compiler/pre-transform/codemods/component-meta-to-define-meta.test.ts
+++ src/compiler/pre-transform/codemods/component-meta-to-define-meta.test.ts
@@ -1,4 +1,3 @@
-import pkg from '@storybook/addon-svelte-csf/package.json' with { type: 'json' };
 import type { Comment, Component } from 'svelte/compiler';
 import { print } from 'svelte-ast-print';
 import { describe, it } from 'vitest';
@@ -11,7 +10,7 @@ describe(transformComponentMetaToDefineMeta.name, () => {
   it('works with a simple example', async ({ expect }) => {
     const code = `
       <script context="module">
-        import { Meta } from "${pkg.name}";
+        import { Meta } from "@storybook/addon-svelte-csf";
       </script>
 
       <Meta title="Atoms/Button" component={Button} />
@@ -32,7 +31,7 @@ describe(transformComponentMetaToDefineMeta.name, () => {
   it('leading comments are included', async ({ expect }) => {
     const code = `
       <script context="module">
-        import { Meta } from "${pkg.name}";
+        import { Meta } from "@storybook/addon-svelte-csf";
       </script>
 
 			<!-- This is a description for the **Button** component stories. -->
@@ -52,6 +51,67 @@ describe(transformComponentMetaToDefineMeta.name, () => {
       )
     ).toMatchInlineSnapshot(`
       "/** This is a description for the **Button** component stories. */ const { Story } = defineMeta({ title: "Atoms/Button", component: Button });"
+    `);
+  });
+
+  it('supports <Meta> parameters with functions', async ({ expect }) => {
+    const code = `
+      <script context="module">
+        import { Meta } from "@storybook/addon-svelte-csf";
+        import WithParameters from './WithParameters.svelte';
+      </script>
+
+      <Meta component={WithParameters} parameters={{
+          docs: {
+            source: {
+              transform: (code) => {
+                return code + 'transformed';
+              }
+            }
+          }
+        }} />
+    `;
+    const component = await parseAndExtractSvelteNode<Component>(code, 'Component');
+
+    expect(
+      print(
+        transformComponentMetaToDefineMeta({ component })
+      )
+    ).toMatchInlineSnapshot(`
+      "const { Story } = defineMeta({
+      	component: WithParameters,
+      	parameters: {
+      		docs: {
+      			source: {
+      				transform: (code) => {
+      					return code + 'transformed';
+      				}
+      			}
+      		}
+      	}
+      });"
+    `);
+  });
+
+  it('supports <Meta> with parameters being referenced to variable in the instance tag', async ({ expect }) => {
+    const code = `
+      <script>
+        const parameters = { foo: 'bar' };
+      </script>
+
+      <Meta component={WithParameters} parameters={{ ...parameters, baz: 'yes'}} />
+    `;
+    const component = await parseAndExtractSvelteNode<Component>(code, 'Component');
+
+    expect(
+      print(
+        transformComponentMetaToDefineMeta({ component })
+      )
+    ).toMatchInlineSnapshot(`
+      "const { Story } = defineMeta({
+      	component: WithParameters,
+      	parameters: { ...parameters, baz: 'yes' }
+      });"
     `);
   });
 });
