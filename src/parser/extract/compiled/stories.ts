57c3b68e34600164541c3aa64b21cde1420e444d
Mateusz Kadlubowski <xeho91@pm.me>
2024-08-12 19:18:20 +0800

    Simplify walk on compiled Stories component default function

--- src/parser/extract/compiled/stories.ts
+++ src/parser/extract/compiled/stories.ts
@@ -10,9 +10,6 @@ interface Params {
 
 type Result = (CallExpression | ExpressionStatement)[];
 
-/**
- * WARN: The content of this function body differs in the production/development mode
- */
 export async function extractStoriesNodesFromExportDefaultFn(params: Params) {
   const { walk } = await import('zimmerframe');
 
@@ -20,32 +17,8 @@ export async function extractStoriesNodesFromExportDefaultFn(params: Params) {
   const { storiesFunctionDeclaration, storyIdentifier } = nodes;
   const state: Result = [];
   const visitors: Visitors<Node, typeof state> = {
-    ExpressionStatement(node, { state, next }) {
-      if (process.env.NODE_ENV !== 'development') {
-        next();
-      }
-
-      // 🫠 ... some fun with AST
-      // Good to know:
-      // I can't split into smaller if-statements,
-      // because TypeScript wouldn't understand that `next()` gets out of the currently visited node scope - aka `return;`
-      if (
-        node.expression.type === 'CallExpression' &&
-        node.expression.callee.type === 'CallExpression' &&
-        node.expression.callee.callee.type === 'MemberExpression' &&
-        node.expression.callee.callee.object.type === 'Identifier' &&
-        node.expression.callee.callee.object.name === '$' &&
-        node.expression.callee.arguments[0].type === 'Identifier' &&
-        node.expression.callee.arguments[0].name === storyIdentifier.name
-      ) {
-        state.push(node);
-      }
-    },
-
-    CallExpression(node, { state, next }) {
-      if (process.env.NODE_ENV !== 'production') {
-        next();
-      }
+    CallExpression(node, context) {
+      const { state } = context;
 
       if (node.callee.type === 'Identifier' && node.callee.name === storyIdentifier.name) {
         state.push(node);
