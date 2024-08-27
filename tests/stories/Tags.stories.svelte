e4feeae89ba1a344d917dcad3c7a493a1e9a721c
Jeppe Reinhold <jeppe@reinhold.is>
2024-08-27 09:11:42 +0200

    document expected tags behavior in tests

--- tests/stories/Tags.stories.svelte
+++ tests/stories/Tags.stories.svelte
@@ -1,6 +1,11 @@
 <script context="module">
   import { defineMeta } from '@storybook/addon-svelte-csf';
 
+  /**
+   * Testing tags parsing in the addon's indexer.
+   * - In the sidebar, only the _"With Dev"_ story should be visible.
+   * - In docs, only the _"With Autodocs"_ story should be visible.
+   */
   const { Story } = defineMeta({
     title: 'Tags',
     parameters: {
@@ -10,10 +15,11 @@
   });
 </script>
 
-<Story name="No Tags">No tags</Story>
 
 <Story name="With Autodocs" tags={['autodocs']}>With autodocs</Story>
 
 <Story name="With Dev" tags={['dev']}>With dev</Story>
 
 <Story name="With Test" tags={['test']}>With test</Story>
+
+<Story name="No Tags">No tags</Story>
