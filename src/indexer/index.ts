acd17d16c0486c0a2d2bc578e3806eb214e50cab
Mateusz Kadlubowski <xeho91@pm.me>
2024-12-13 23:40:02 +0800

    fix: Prevent parser indexer not letting other addon errors to throw (#242)

--- src/indexer/index.ts
+++ src/indexer/index.ts
@@ -9,12 +9,15 @@ import {
 } from '#utils/error/parser/extract/svelte';
 import { LegacyTemplateNotEnabledError } from '#utils/error/legacy-api/index';
 import { NoDestructuredDefineMetaCallError } from '#utils/error/parser/analyse/define-meta';
+import { isStorybookSvelteCSFError } from '#utils/error';
 
 export const createIndexer = (legacyTemplate: boolean): Indexer => ({
   test: /\.svelte$/,
   createIndex: async (filename, { makeTitle }) => {
     try {
-      const { meta, stories } = await parseForIndexer(filename, { legacyTemplate });
+      const { meta, stories } = await parseForIndexer(filename, {
+        legacyTemplate,
+      });
 
       return stories.map((story) => {
         return {
@@ -36,10 +39,15 @@ export const createIndexer = (legacyTemplate: boolean): Indexer => ({
         error instanceof GetDefineMetaFirstArgumentError
       ) {
         const { filename } = error;
-        throw new LegacyTemplateNotEnabledError(filename);
+        throw new LegacyTemplateNotEnabledError(filename, { cause: error });
       }
 
-      throw new IndexerParseError();
+      // WARN: We can't use `instanceof StorybookSvelteCSFError`, because is an _abstract_ class
+      if (isStorybookSvelteCSFError(error)) {
+        throw error;
+      }
+
+      throw new IndexerParseError({ cause: error });
     }
   },
 });
