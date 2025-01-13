31bd578bcb0f9c99dde4e953bd2add00c323125d
Jeppe Reinhold <jeppe@reinhold.is>
2025-01-13 21:50:45 +0100

    remove generated file from git

--- tests/extractor.d.ts
+++
@@ -1,8 +0,0 @@
-import { type SvelteAST } from '$lib/parser/ast.js';
-export declare function extractSvelteNode<TNode extends SvelteAST.SvelteNode | SvelteAST.Script>(
-  parsed: SvelteAST.SvelteNode | SvelteAST.Script,
-  name: TNode['type']
-): Promise<TNode>;
-export declare function parseAndExtractSvelteNode<
-  TNode extends SvelteAST.SvelteNode | SvelteAST.Script,
->(code: string, name: TNode['type']): Promise<TNode>;
